061a98e6e8e89701e8c6a134a37f81157f26beb0
PolyVector <PolyVector@protonmail.com>
2026-01-10 17:21:23 +0000

    services

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
+use std::error;
+
 use axum::{Router, response::Html, routing::get};
-use maud::{Markup, html};
+use maud::{Markup, Render, html};
+use tower::ServiceBuilder;
+use tower_http::services::ServeDir;
 
 use tokio;
 
 async fn handler() -> Markup {
     html! {
+        link rel="stylesheet" type="text/css" href="assets/css/style.css";
+
         h1 { "Hello, world!" }
+
         p.intro {
             "This is an example of the "
             a href="https://github.com/lambda-fairy/maud" { "Maud" }
@@ -15,13 +22,16 @@ async fn handler() -> Markup {
 }
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(handler));
+async fn main() -> Result<(), Box<dyn error::Error>> {
+    let app = Router::new().route("/", get(handler)).nest_service(
+        "/assets",
+        ServiceBuilder::new().service(ServeDir::new("assets")),
+    );
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await?;
 
-    println!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await;
+    println!("Listening on {}", listener.local_addr()?);
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
