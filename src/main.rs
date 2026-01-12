44814588bcec3842556f56f435331386f94000c2
petally <petalimium@gmail.com>
2026-01-12 18:41:20 +0200

    refactor
    i basically just copied the sqlx todos example but made it fit. this still needs a little more cleaning up but it's a lot more readable i think

--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,5 @@
-use maud::{DOCTYPE, Markup, html};
-use sqlx::postgres::{PgPoolOptions, PgRow};
-use sqlx::prelude::FromRow;
-use sqlx::{Error, Pool, Postgres, query_file};
-use std::convert::Infallible;
+use sqlx::postgres::PgPoolOptions;
 use std::env;
-use std::fmt::Display;
-use warp::Filter;
-
-#[derive(FromRow)]
-struct BoardQuery {
-    pub id: i64,
-    pub name: String,
-    pub description: String,
-}
-
-impl Display for BoardQuery {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            r#"
-                id: {},
-                name: {},
-                description: {}
-            "#,
-            self.id, self.name, self.description
-        )
-    }
-}
-
-// This doesnt need to be in a function
-// but I'd like to try since we'll probably
-// need to put stuff like this in functions later
-async fn get_boards(conn: &sqlx::Pool<sqlx::Postgres>) -> Result<Vec<BoardQuery>, Error> {
-    let rows = sqlx::query_as!(BoardQuery, "SELECT * FROM boards ORDER BY name")
-        .fetch_all(conn)
-        .await?;
-    Ok(rows)
-}
-
-fn header(page_title: &str) -> Markup {
-    html! {
-        (DOCTYPE)
-        meta charset="utf-8";
-        title { (page_title) }
-        link rel="stylesheet" href="assets/css/style.css";
-    }
-}
-
-fn page(title: &str) -> Markup {
-    html! {
-        (header(title))
-        h1 { (title) }
-    }
-}
-
-fn boards_partial(boards: Result<Vec<BoardQuery>, Error>) -> Markup {
-    match boards {
-        Ok(boards) => {
-            html! {
-                @for board in boards {
-                    a href="/b/g" { (board) }
-                }
-            }
-        }
-        Err(_) => {
-            html! {
-                p { "No boards found" }
-            }
-        }
-    }
-}
-
-async fn index_page(boards: Markup) -> Result<Markup, Infallible> {
-    Ok(html! {
-        (page("brd"))
-        p { "welcome to brd" }
-
-        h2 { "boards" }
-        (boards)
-
-        h2 { "description" }
-        p { "A simple imageboard site that supports private walled-garden communication" }
-    })
-}
-
-fn with_pool(
-    pool: Pool<Postgres>,
-) -> impl Filter<Extract = (Pool<Postgres>,), Error = std::convert::Infallible> + Clone {
-    warp::any().map(move || pool.clone())
-}
-
-// TODO: potentially put this in a handler module
-async fn index(pool: Pool<Postgres>) -> Result<impl warp::Reply, Infallible> {
-    index_page(boards_partial(get_boards(&pool).await)).await
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -106,41 +12,125 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .connect(&env::var("DATABASE_URL")?)
         .await?;
 
-    query_file!("queries/seed_data.sql").execute(&pool).await?;
+    let api = filters::api(pool);
 
-    // Make a simple query to return the given parameter
-    let row: (i64,) = sqlx::query_as("SELECT $1")
-        .bind(150_i64)
-        .fetch_one(&pool)
-        .await?;
-
-    assert_eq!(row.0, 150);
-
-    let index_route = warp::path::end()
-        .and(with_pool(pool.clone()))
-        .and_then(index);
-
-    let board_route = warp::path("b")
-        .and(warp::path::param())
-        .map(|param: String| page(&param));
-
-    let user_route = warp::path("u")
-        .and(warp::path::param())
-        .map(|param: String| page(&format!("user {}", param)));
-
-    #[rustfmt::skip]
-    let static_assets_route = warp::path("assets")
-        .and(warp::fs::dir("assets"));
-
-    #[rustfmt::skip]
-    let routes = warp::get().and(
-        index_route
-            .or(board_route)
-            .or(user_route)
-            .or(static_assets_route)
-    );
-
-    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
+    warp::serve(api).run(([127, 0, 0, 1], 8000)).await;
 
     Ok(())
+}
+
+mod filters {
+    use super::db::with_pool;
+    use super::handlers;
+    use sqlx::{Pool, Postgres};
+    use warp::Filter;
+
+    // All the routes combined
+    pub fn api(
+        pool: Pool<Postgres>,
+    ) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
+        boards_list(pool.clone())
+    }
+
+    pub fn boards_list(
+        pool: Pool<Postgres>,
+    ) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
+        warp::path::end()
+            .and(warp::get())
+            .and(with_pool(pool))
+            .and_then(handlers::index)
+    }
+}
+
+mod handlers {
+    use super::db::boards_get;
+    use super::views::{boards_partial, index_page};
+    use sqlx::{Pool, Postgres};
+    use std::convert::Infallible;
+
+    pub async fn index(pool: Pool<Postgres>) -> Result<impl warp::Reply, Infallible> {
+        index_page(boards_partial(boards_get(pool).await))
+    }
+}
+
+mod models {
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Debug, Deserialize, Serialize, Clone)]
+    pub struct Board {
+        pub id: i64,
+        pub name: String,
+        pub description: String,
+    }
+}
+
+mod views {
+    use super::models::Board;
+    use maud::{DOCTYPE, Markup, html};
+    use std::convert::Infallible;
+
+    pub fn header(page_title: &str) -> Markup {
+        html! {
+            (DOCTYPE)
+            meta charset="utf-8";
+            title { (page_title) }
+            link rel="stylesheet" href="assets/css/style.css";
+        }
+    }
+
+    pub fn page(title: &str) -> Markup {
+        html! {
+            (header(title))
+            h1 { (title) }
+        }
+    }
+
+    pub fn boards_partial(boards: Result<Vec<Board>, sqlx::Error>) -> Markup {
+        match boards {
+            Ok(boards) => {
+                html! {
+                    @for board in boards {
+                        a href="/b/g" { (board.name) }
+                    }
+                }
+            }
+            Err(_) => {
+                html! {
+                    p { "No boards found" }
+                }
+            }
+        }
+    }
+
+    pub fn index_page(boards: Markup) -> Result<Markup, Infallible> {
+        Ok(html! {
+            (page("brd"))
+            p { "welcome to brd" }
+
+            h2 { "boards" }
+            (boards)
+
+            h2 { "description" }
+            p { "A simple imageboard site that supports private walled-garden communication" }
+        })
+    }
+}
+
+mod db {
+    use super::models::Board;
+    use sqlx::{Pool, Postgres};
+    use std::convert::Infallible;
+    use warp::Filter;
+
+    pub fn with_pool(
+        pool: Pool<Postgres>,
+    ) -> impl Filter<Extract = (Pool<Postgres>,), Error = Infallible> + Clone {
+        warp::any().map(move || pool.clone())
+    }
+
+    pub async fn boards_get(pool: Pool<Postgres>) -> Result<Vec<Board>, sqlx::Error> {
+        sqlx::query_as!(Board, "SELECT * FROM boards ORDER BY name")
+            .fetch_all(&pool)
+            .await
+    }
 }
